d97220197820ce6147e9bb45accda080c04441c2
craigcalzado <craig.a.calzado@gmail.com>
2022-01-19 09:54:42 -0600

    updated data_types_and_variables.py

---
+++ data_types_and_variables.py
@@ -0,0 +1,69 @@
+
+from concurrent.futures.process import _ThreadWakeup
+from re import T
+
+# 1.
+the_little_mermaid = 3 #days
+brother_bear = 5 #days
+hercules = 1 #days
+cost_per_movie = 3 # dollars
+
+total_days_rented = the_little_mermaid + brother_bear + hercules
+
+total_cost = total_days_rented * cost_per_movie
+print(total_cost)
+
+
+google = 400
+amazon = 380
+facebook = 350
+
+payment = (facebook * 10) + (google * 6) + (amazon * 6)
+
+print(payment)
+
+# 2.
+class_closed = False
+class_open = True
+schedule_conflicts = False
+schedule_not_conflicts = True
+
+class_open and schedule_not_conflicts
+class_open and schedule_conflicts
+class_closed or schedule_conflicts
+
+# 3.
+items_in_cart = 4
+
+min_items = 3
+
+offer_expired = False
+offer_not_expired = True
+premium_member = True
+not_premium_member = False
+
+items_in_cart >= min_items and offer_not_expired
+items_in_cart >= min_items or premium_member and offer_expired
+
+# 4.
+
+username = 'codeup'
+password = 'notastrongpassword'
+
+min_pass_char = 5
+max_user_char = 20
+same_info = username == password
+ 
+pass_char = len(password) >= min_pass_char
+user_char = len(username) < max_user_char
+
+success = pass_char and user_char and not same_info
+
+print(success)
+
+# Bonus
+user_space = username[0] == ' ' or username[-1] == ' '
+pass_space = password[0] == ' ' or password[-1] == ' '
+
+success = pass_char and user_char and not same_info and not (user_space or pass_space)
+
